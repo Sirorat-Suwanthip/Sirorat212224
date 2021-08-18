6ca3e2fc9ad7c784b3b972db3e44350798c9d052
Sirorat-Suwanthip <71056545+Sirorat-Suwanthip@users.noreply.github.com>
2021-08-18 19:08:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <h1> Sirorat Suwanthip 62109459 </h1> 
 
-<ifram src ="https://thingspeak.com/channels/1458416/maps/channel_show">
+<ifram src ="https://thingspeak.com/channels/1458416/maps/channel_show" </ifram>

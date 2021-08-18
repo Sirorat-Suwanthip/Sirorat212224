292be523c57a60f1f68f0f691003f6e2369d4590
Sirorat-Suwanthip <71056545+Sirorat-Suwanthip@users.noreply.github.com>
2021-08-18 19:05:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <h1> Sirorat Suwanthip 62109459 </h1> 
 
-<ifram src = "https://thingspeak.com/channels/1458416/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">
+<ifram src ="https://thingspeak.com/channels/1458416/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">

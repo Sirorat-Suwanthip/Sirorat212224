4866579ffe1c1e36a350a488604fa0df7365c50c
Sirorat-Suwanthip <71056545+Sirorat-Suwanthip@users.noreply.github.com>
2021-08-18 20:23:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -69,8 +69,8 @@ footer {
 </header>
 <section>
  <article>
-    <iframe src="https://thingspeak.com/channels/1458416/maps/channel_show" width="450" height="260">
-<iframe src="https://thingspeak.com/channels/1458416/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="260">
+    <iframe src="https://thingspeak.com/channels/1458416/maps/channel_show" width="450" height="260"></iframe>
+<iframe src="https://thingspeak.com/channels/1458416/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="260"></iframe>
 <iframe src="https://thingspeak.com/channels/1458416/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="260"></iframe>
 <?php
  $Temperature = file_get_contents('https://api.thingspeak.com/channels/1458415/fields/1/last.txt');

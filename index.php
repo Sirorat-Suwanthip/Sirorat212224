6d4f89af89238c4a986f0644e542803d5f0b46ae
Sirorat-Suwanthip <71056545+Sirorat-Suwanthip@users.noreply.github.com>
2021-08-18 20:40:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -63,11 +63,13 @@ footer {
 </header>
    
 <nav>
+  <table>
     <tr>
       <td><iframe src="https://thingspeak.com/channels/1458416/maps/channel_show" width="450" height="260"></iframe></td>
       <td><iframe src="https://thingspeak.com/channels/1458416/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="260"></iframe></td>
       <td><iframe src="https://thingspeak.com/channels/1458416/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="260"></iframe></td>
     </tr>
+    </table>
 <?php
  $Temperature = file_get_contents('https://api.thingspeak.com/channels/1458415/fields/1/last.txt');
  $Humidity = file_get_contents('https://api.thingspeak.com/channels/1458415/fields/2/last.txt');
@@ -78,11 +80,13 @@ function timedRefresh(timeoutPeriod) {
 }
 </script>
 </nav>
+   <table>
    <tr>
      <td>My home location</td>
      <td><br> <?php echo "Humidity is = ".$Temperature ?> </br> </td>
      <td><br> <?php echo " Temperature is = ".$Humidity ?> </br></td>
     </tr>
+   </table>
 
 <footer>
   <p>Happy Happy</p>
